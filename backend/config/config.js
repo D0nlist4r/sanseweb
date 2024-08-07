61d4631549ddc51b6b4d9d4fe560bd07b3e90a65
diegomao <diegomcode@gmail.com>
2024-08-07 13:10:36 -0500

    se añade soporte para puerto en mysql en db

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,6 +2,7 @@ import dotenv  from 'dotenv';
 dotenv.config();
 
 const config = {
+    port: process.env.PORT_DB,
     host: process.env.HOST,
     user: process.env.USERNAME1,
     password: process.env.PASSWORD,
